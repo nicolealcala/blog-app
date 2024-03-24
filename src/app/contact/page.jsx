1ab2923af084e83aed6f94b98477cfaf59e8d901
Nicole Alcala <nicolealcalq@gmail.com>
2024-03-24 19:08:23 +0800

    contact page update

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,82 @@
+import Image from "next/image";
+
 const Contact = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className="row mx-0">
+      <div className="col-lg-6 d-none d-lg-flex justify-content-center align-items-center">
+        <div className="img-container">
+          <a href="https://storyset.com/business" target="_blank">
+            <Image
+              src="/contact.png"
+              alt="About Image"
+              fill
+              className="img-contain"
+            />
+          </a>
+        </div>
+      </div>
+      <div className="col-lg-6 d-flex flex-column justify-content-center align-items-center py-5">
+        <h3 className="mb-4 d-lg-none txt-color-mid">Contact Us</h3>
+        <form action="" data-bs-theme="dark">
+          <div className="row mx-0">
+            <div className="col-md-6 mb-2">
+              <label htmlFor="fName" className="text-muted">
+                First Name
+              </label>
+              <input type="text" className="form-control" id="fName" required />
+            </div>
+            <div className="col-md-6 mb-2">
+              <label htmlFor="lName" className="text-muted">
+                Last Name
+              </label>
+              <input type="text" className="form-control" id="lName" required />
+            </div>
+            <div className="col-12 mb-2">
+              <label htmlFor="contact" className="text-muted">
+                Phone Number (optional)
+              </label>
+              <input
+                type="tel"
+                maxLength={11}
+                className="form-control"
+                placeholder="09XXXXXXXXX"
+                id="contact"
+              />
+            </div>
+            <div className="col-12 mb-2">
+              <label htmlFor="email" className="text-muted">
+                Email
+              </label>
+              <input
+                type="email"
+                maxLength={50}
+                className="form-control"
+                placeholder="example@example.com"
+                id="email"
+                required
+              />
+            </div>
+            <div className="col-12 mb-2">
+              <label htmlFor="email" className="text-muted">
+                Message
+              </label>
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="8"
+                required
+              ></textarea>
+            </div>
+            <div className="col-12 my-2">
+              <button type="submit" className="primary-btn">
+                Send message
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
