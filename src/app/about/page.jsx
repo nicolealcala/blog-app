668d595bad9ff7ac0da93b990f352e6c4d9e035e
Nicole Alcala <nicolealcalq@gmail.com>
2024-03-24 19:07:43 +0800

    about page updates

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const About = () => {
   return (
     <div className="row mx-0">
-      <div className="col-lg-6 d-flex flex-column justify-content-evenly">
+      <div className="col-lg-6 d-flex flex-column justify-content-evenly py-5 gap-4">
         <div className="row mx-0">
           <div className="col-12">
             <h4 className="txt-color-mid txt-weight-mid">About Agency</h4>
@@ -18,9 +18,11 @@ const About = () => {
           </div>
           <div className="col-12">
             <p className="mt-3">
-              We believe in good ideas flexibility and precission We’re world’s
-              Our Special Team best consulting & finance solution provider. Wide
-              range of web and software development services.
+              We specialize in crafting digital concepts that transcend the
+              ordinary. With a commitment to pushing boundaries, we strive to
+              deliver innovative solutions that captivate audiences and leave a
+              lasting impression. Our mission is to redefine what&#39;s possible
+              in the digital landscape, one bold idea at a time.
             </p>
           </div>
         </div>
@@ -30,16 +32,16 @@ const About = () => {
             <p>Partner companies</p>
           </div>
           <div className="col-4">
-            <h1 className="txt-color-mid txt-weight-mid">1000+</h1>
-            <p>Community members</p>
+            <h1 className="txt-color-mid txt-weight-mid">4k+</h1>
+            <p>Happy dreamers</p>
           </div>
           <div className="col-4">
-            <h1 className="txt-color-mid txt-weight-mid">2000+</h1>
-            <p>Creative inspirations</p>
+            <h1 className="txt-color-mid txt-weight-mid">2k+</h1>
+            <p>Creative solutions</p>
           </div>
         </div>
       </div>
-      <div className="col-lg-6 d-flex justify-content-center align-items-center">
+      <div className="col-lg-6 d-none d-lg-flex justify-content-center align-items-center">
         <div className="img-container">
           <a href="https://storyset.com/work" target="_blank">
             <Image
