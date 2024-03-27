5d9f5ffa7fadcafcd711113466de7a070b6b27ca
Nicole Alcala <nicolealcalq@gmail.com>
2024-03-27 16:31:01 +0800

    password not required for socials login, default image

--- src/lib/models.js
+++ src/lib/models.js
@@ -3,15 +3,15 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true, min: 3, max: 20 },
     email: { type: String, required: true, unique: true, max: 50 },
-    password: { type: String, required: true, min: 8 },
-    img: { type: String },
+    password: { type: String },
+    img: { type: String, default: "/noavatar.png" },
     isAdmin: { type: Boolean, default: false }
 }, { timeStamps: true });
 
 const blogSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true, },
-    img: { type: String },
+    img: { type: String, default: "/blog.png" },
     userId: { type: String, requireD: true, },
     slug: { type: String, requireD: true, unique: true }
 }, { timeStamps: true });
