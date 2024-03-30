de89485d39b972d3bab0e5d8415aaaf9c9adf8f7
Nicole Alcala <nicolealcalq@gmail.com>
2024-03-30 18:23:29 +0800

    postcard date

--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -15,11 +15,14 @@ const PostCard = ({ item }) => {
             className="img-cover"
           />
         </div>
-        <span className={styles.date}>03.23.24</span>
+        <span className={styles.date}>{item.createdAt}</span>
       </div>
       <div>
-        <h5 className={styles.title}>{item.title}</h5>
-        <p className={styles.desc}>{item.body}</p>
+        <h6 className={styles.title}>{item.title}</h6>
+        <p className={styles.desc}>
+          {item.content.slice(0, 80)}
+          {item.content.length > 80 ? "..." : ""}
+        </p>
         <Link href={`/blogs/${item.slug}`} className={styles.link}>
           READ MORE
         </Link>
