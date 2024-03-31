e24957d2d0c8ec4702332c9bfa11eab0a4dc85cc
Nicole Alcala <nicolealcalq@gmail.com>
2024-03-31 14:49:48 +0800

    postcard date and desc rendeing updated

--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -1,29 +1,42 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./postcard.module.css";
+import { createMarkup } from "@/lib/actions";
 
+const truncateContent = (content) => {
+  if (content.length > 80) {
+    return content.slice(0, 80) + "...";
+  }
+  return content;
+};
+
+const removeHtmlTags = (str) => {
+  return str.replace(/<[^>]*>/g, "");
+};
 const PostCard = ({ item }) => {
+  const options = { month: "long", day: "numeric", year: "numeric" };
+
   return (
     <div className={styles.card}>
-      <div className={styles.top}>
-        <div className={styles.imgContainer}>
-          <Image
-            src={item.img ? item.img : "/blog.png"}
-            alt="Post-Img"
-            fill
-            sizes="100vw"
-            className="img-cover"
-          />
-        </div>
-        <span className={styles.date}>{item.createdAt}</span>
+      <div className={styles.imgContainer}>
+        <Image
+          src={item?.img}
+          alt="Post-Img"
+          fill
+          sizes="100vw"
+          className="img-cover"
+        />
       </div>
       <div>
-        <h6 className={styles.title}>{item.title}</h6>
+        <span className={styles.date}>
+          {new Date(item.createdAt).toLocaleDateString("en-US", options)}
+        </span>
+        <h6 className={styles.title}>{item?.title}</h6>
         <p className={styles.desc}>
-          {item.content.slice(0, 80)}
-          {item.content.length > 80 ? "..." : ""}
+          {truncateContent(removeHtmlTags(item.content))}
         </p>
-        <Link href={`/blogs/${item.slug}`} className={styles.link}>
+
+        <Link href={`/blogs/${item?.slug}`} className={styles.link}>
           READ MORE
         </Link>
       </div>
