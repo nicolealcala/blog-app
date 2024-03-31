d3a4d905a57ce180ac54078cb9b3c44dcc8d695d
Nicole Alcala <nicolealcalq@gmail.com>
2024-03-31 14:48:48 +0800

    fixed markup rendering

--- src/app/blogs/[slug]/page.jsx
+++ src/app/blogs/[slug]/page.jsx
@@ -3,6 +3,8 @@ import styles from "./blog.module.css";
 import Author from "@/components/postAuthor/Author";
 import { Suspense } from "react";
 import { getBlog } from "@/lib/data";
+import NotFound from "@/app/not-found";
+import { createMarkup } from "@/lib/actions";
 
 //Next.js only fetch data once even if its called by multiple functions (generateMetaData)
 export const dynamicMetadata = async ({ params }) => {
@@ -21,17 +23,13 @@ const BlogPost = async ({ params }) => {
 
   // FETCH DATA WITHOUT API
   const blog = await getBlog(slug);
+  if (!blog) return <NotFound />;
 
   return (
     <div className="row mx-0">
       <div className="col-3 d-none d-lg-flex p-0">
         <div className={styles.postImg}>
-          <Image
-            src={blog.img ? blog.img : "/blog.png"}
-            alt="Post Image"
-            fill
-            className="img-cover"
-          />
+          <Image src={blog.img} alt="Post Image" fill className="img-cover" />
         </div>
       </div>
       <div
@@ -41,7 +39,10 @@ const BlogPost = async ({ params }) => {
         <Suspense fallback={<div>Loading...</div>}>
           <Author userId={blog.userId} />
         </Suspense>
-        <p className="mt-5">{blog.content}</p>
+        <div
+          className="mt-5"
+          dangerouslySetInnerHTML={createMarkup(blog.content)}
+        ></div>
       </div>
     </div>
   );
