cae41ea00f9ce4726968dc5b17ebb6c6e54b4c90
Nicole Alcala <nicolealcalq@gmail.com>
2024-04-03 15:54:20 +0800

    updates on comment section

--- src/app/blogs/[slug]/page.jsx
+++ src/app/blogs/[slug]/page.jsx
@@ -23,70 +23,75 @@ export const dynamicMetadata = async ({ params }) => {
 
 const BlogPost = async ({ params }) => {
   const { slug } = params;
+  const session = await auth();
 
   // FETCH DATA WITHOUT API
   const blog = await getBlog(slug);
   if (!blog) return <NotFound />;
 
-  const session = await auth();
-  const comments = await getComments();
+  const comments = await getComments(blog.id);
 
   return (
-    <div className="row mx-0 pb-5">
-      <div className="col-12 d-flex p-0">
-        <div className={styles.postImg}>
-          <Image src={blog.img} alt="Post Image" fill className="img-cover" />
-        </div>
-      </div>
-      <div className={`row mx-0 pt-3 ${styles.content}`}>
-        <div className="col-12">
-          <h2>
-            <strong>{blog.title}</strong>
-          </h2>
-        </div>
-        <div className="col-12 col-lg-6">
-          <Suspense fallback={<div>Loading...</div>}>
-            <Author userId={blog.userId} date={blog.createdAt} />
-          </Suspense>
-        </div>
-        {session?.user.id === blog.userId && (
-          <div className="col-12 col-lg-6 d-flex justify-content-end align-items-center">
-            <button className="btn btn-primary me-3">Edit</button>
-            <form action={deleteBlog}>
-              <button className="btn btn-danger">Delete</button>
-            </form>
+    <div className={`row mx-0 pb-5 ${styles.body}`}>
+      <div className="postBody p-3">
+        <div className="col-12 d-flex p-0">
+          <div className={styles.postImg}>
+            <Image src={blog.img} alt="Post Image" fill className="img-cover" />
           </div>
-        )}
-        <div
-          className="col-12 mt-5"
-          dangerouslySetInnerHTML={createMarkup(blog.content)}
-        ></div>
-        <hr className="mt-4 mb-0" />
-        <div
-          className="row mx-0 pt-4 pb-2 rounded-bottom-2"
-          style={{ backgroundColor: "var(--bg-dark)" }}
-        >
+        </div>
+        <div className={`row mx-0 pt-3 ${styles.content}`}>
           <div className="col-12">
-            <CommentForm session={session} blogId={blog.id} />
+            <h2>
+              <strong>{blog.title}</strong>
+            </h2>
           </div>
-
-          {comments.length > 0 && (
-            <>
-              <div className="col-12 txt-color-soft my-3">
-                {comments.length > 0
-                  ? "Comments"
-                  : "This post has no comments yet."}
-              </div>
-              {comments.map((comment) => (
-                <>
-                  <CommentBox comment={comment} key={comment.id} />
-                  <hr className="mt-2" />
-                </>
-              ))}
-            </>
+          <div className="col-12 col-lg-6">
+            <Suspense fallback={<div>Loading...</div>}>
+              <Author userId={blog.userId} date={blog.createdAt} />
+            </Suspense>
+          </div>
+          {session?.user.id === blog.userId && (
+            <div className="col-12 col-lg-6 d-flex justify-content-end align-items-center">
+              <button className="btn btn-primary me-3">Edit</button>
+              <form action={deleteBlog}>
+                <button className="btn btn-danger">Delete</button>
+              </form>
+            </div>
           )}
+          <div
+            className={`col-12 mt-5`}
+            dangerouslySetInnerHTML={createMarkup(blog.content)}
+          ></div>
         </div>
       </div>
+      <hr className="mt-4 mb-0" />
+      {comments.length === 0 && (
+        <div className="col-12 txt-color-soft mt-3">
+          This post has no comments yet.
+        </div>
+      )}
+      <div className="col-12 mt-3">
+        <CommentForm session={session} blogId={blog.id} />
+      </div>
+
+      {comments.length > 0 && (
+        <>
+          <div className="col-12 txt-color-soft my-3">
+            {comments.length > 0 && "Comments"}
+          </div>
+          {comments.map((comment) => (
+            <>
+              <CommentBox
+                session={session}
+                comment={comment}
+                poster={blog.userId}
+                key={comment.id}
+              />
+              <hr className="my-2" />
+            </>
+          ))}
+        </>
+      )}
     </div>
   );
 };
