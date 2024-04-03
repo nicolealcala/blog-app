753b39de238fa15a07407089343ef4379d6aa735
Nicole Alcala <nicolealcalq@gmail.com>
2024-04-03 15:53:35 +0800

    updated comment box and comment form

--- src/components/comments/comment.jsx
+++ src/components/comments/comment.jsx
@@ -1,20 +1,43 @@
 import { getUser } from "@/lib/data";
 import Image from "next/image";
-const CommentBox = async ({ comment }) => {
-  console.log(comment.userId);
+import styles from "./comment.module.css";
+
+const CommentBox = async ({ session, comment, poster }) => {
   const user = await getUser(comment?.userId);
+
   return (
-    <div className="d-flex">
+    <div className="d-flex commentForm" data-bs-theme="dark">
       <Image
         src={user?.img}
         width={35}
         height={35}
         alt="Commentor Image"
-        className="rounded-pill me-2"
+        className="rounded-pill"
       />
-      <div className="">
-        <p className="my-0 txt-color-soft txt-size-small">{user.username}</p>
-        <p className="my-0">{comment?.content}</p>
+      <div className="d-flex flex-column w-100 ms-3">
+        <p className="my-0 txt-color-soft txt-size-mid">
+          <strong>{user?.username}</strong>
+          {poster === comment.userId && <> &#x2022; Author</>}
+        </p>
+        <div className="col-12">
+          <p className="form-control commentBox my-2">{comment?.content}</p>
+          {session?.user.id === comment.userId && (
+            <div className="d-flex align-items-center">
+              <button
+                className="bg-transparent border-0 txt-size-mid me-3"
+                id={styles.edit}
+              >
+                Edit
+              </button>
+              <button
+                className="bg-transparent border-0 txt-size-mid"
+                id={styles.delete}
+              >
+                Delete
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
