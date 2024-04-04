9e72d5d76c60150df46cf6048190584fa9c1a8f2
Nicole Alcala <nicolealcalq@gmail.com>
2024-04-04 12:36:43 +0800

    toggle modal updates

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -38,7 +38,11 @@ const Blogs = async () => {
           <>
             {" "}
             <p>Come back later or create a new post.</p>
-            <ToggleModal userId={session?.user.id} btnName="+ Create Blog" />
+            <ToggleModal
+              userId={session?.user.id}
+              btnName="+ Create Blog"
+              btnId=""
+            />
             {/* <Modal userId={JSON.parse(JSON.stringify(user._id))} /> */}
           </>
         ) : (
@@ -51,33 +55,16 @@ const Blogs = async () => {
   }
 
   return (
-    <div className="position-relative py-4">
-      <div className="row mx-0 gx-5">
-        <div
-          className={`col-12 mb-3 d-flex align-items-center ${
-            session?.user.isAdmin && "col-lg-6"
-          }`}
-        >
-          <h4 className="my-0">All blogs</h4>
-        </div>
-        {session?.user.isAdmin && (
-          <div className="d-none d-lg-flex justify-content-end col-6 mb-3">
-            <ToggleModal userId={session?.user.id} btnName="+ Create Blog" />
-          </div>
-        )}
+    <div className="row mx-0 gx-5 py-5">
+      {session?.user.isAdmin && (
+        <ToggleModal userId={session?.user.id} btnName="+" btnId="addBtn" />
+      )}
 
-        {blogs.map((blog) => (
-          <div className="col-md-6 col-lg-4 mb-4" key={blog.id}>
-            <PostCard item={blog} />
-          </div>
-        ))}
-      </div>
-      <div
-        className="d-flex d-lg-none bg-light rounded-pill overflow-hidden"
-        id="addBtn"
-      >
-        <ToggleModal userId={session?.user.id} btnName="+" />
-      </div>
+      {blogs.map((blog) => (
+        <div className="col-md-6 col-lg-4 mb-4" key={blog.id}>
+          <PostCard item={blog} />
+        </div>
+      ))}
     </div>
   );
 };
